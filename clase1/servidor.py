de30f2f629b00a93359765da07f5d3b5e0fc94ba
Mauricio Lopez <mlopezcamp.dev@gmail.com>
2026-02-20 20:47:49 -0500

    Agrega conexion concurrente

--- clase1/servidor.py
+++ clase1/servidor.py
@@ -1,13 +1,31 @@
 import socket
+import threading
+
+def handle_client(conn, addr):
+	print(f"cliente conetado desde {addr}")
+
+	try:
+		student_name = conn.recv(1024).decode()
+		response = f"Hola {student_name}, estas conectado a un servidor concurrente"
+		conn.sendall(response.encode())
+	except Exeption as e:
+		print(f"Error con {addr} : {e}")
+	finally:
+		conn.close()
+		print(f"Conexion cerrada {addr}")
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("127.0.1.1", 5000))
+server.bind(("0.0.0.0", 5000))
 server.listen(1)
 
 print("Servidor esperando conexion...")
 
-conn, addr = server.accept()
-print("Cliente conectado:", addr)
-
-conn.sendall(b"Hola Mauricio Lopez")
-conn.close()
+while True:
+	conn, addr = server.accept()
+	
+	client_thread = threading.Thread(
+		target=handle_client,
+		args=(conn,addr)
+	)
+	client_thread.start()
